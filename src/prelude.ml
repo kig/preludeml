8508a24f978bad2fbee178d9363ac0d4c1a2715d
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2009-01-01 13:38:12 +0200

    more unfold tests

--- src/prelude.ml
+++ src/prelude.ml
@@ -375,7 +375,15 @@ let unfoldrWhile = unfoldr
 let unfoldlWhile = unfoldl
 
 let unfoldrUntil p f init = unfoldr (fun v -> not (p v)) f init
+(**T
+  unfoldrUntil (gte 4) (fupler succ) 1 = [3; 2; 1]
+  unfoldrUntil (gte 68) (fuple char succ) 65 = ['C'; 'B'; 'A']
+**)
 let unfoldlUntil p f init = unfoldl (fun v -> not (p v)) f init
+(**T
+  unfoldlUntil (gte 4) (fupler succ) 1 = [1; 2; 3]
+  unfoldlUntil (gte 68) (fuple char succ) 65 = ['A'; 'B'; 'C']
+**)
 
 let unfoldrFilter p s f init =
   let rec aux p f v l =
@@ -393,10 +401,16 @@ let unfoldlFilter p s f init = List.rev (unfoldrFilter p s f init)
   unfoldlFilter (lt 7) even (fuple (divide 2) succ) 2 = [1; 2; 3]
 **)
 
-let unfoldlN f n i =
+let readN f n i =
   unfoldlWhile (fun (_,v) -> v > 0) (fun (s,c) -> (f s, (s, pred c))) (i, n)
+(**T
+  readN (fun i -> i := !i+1; !i) 10 (ref 0) = (1--10)
+**)
 
 let forN f n = for i=0 to (n-1) do f i done
+(**T
+  (let i = ref 0 in forN (fun j -> i := !i + j) 10; !i = sum (0--9))
+**)
 
 let generateOpt f init =
   unfoldlOpt (fun x -> match f x with Some a -> Some (x,a) | None -> None) init
@@ -408,6 +422,9 @@ let generate p f init = unfoldl p (fupler f) init
   generate (lessOrEqualTo 3) succ 1 = [1; 2; 3]
 **)
 let generateUntil p f init = generate (fun v -> not (p v)) f init
+(**T
+  generateUntil (gt 3) succ 1 = [1; 2; 3]
+**)
 
 let generateOptR f init =
   unfoldrOpt (fun x -> match f x with Some a -> Some (x,a) | None -> None) init
@@ -419,6 +436,9 @@ let generateR p f init = unfoldr p (fupler f) init
   generateR (lte 3) succ 1 = [3; 2; 1]
 **)
 let generateUntilR p f init = generateR (fun v -> not (p v)) f init
+(**T
+  generateUntilR (gt 3) succ 1 = [3; 2; 1]
+**)
 
 let iterate f n s =
   let rec aux f n s rv i =
@@ -3270,7 +3290,7 @@ let appendToFile filename str = withFileAppend filename (flip output_string str)
 let readLines = lines @. readFile
 
 let tokenize t ic = unfoldlOpt (maybeEOF None (fun ic -> Some (t ic, ic))) ic
-let tokenizeN t n ic = unfoldlN t n ic
+let tokenizeN t n ic = readN t n ic
 let tokenizeIter t f ic = maybeEOF () (loop (f @. t)) ic
 let tokenizeMap t f ic = tokenize (f @. t) ic
 let tokenizeFile t filename = withFile filename (tokenize t)
